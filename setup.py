7059faab5e1d2e6ecf8f081875e5f096462c25f8
Mohsin-h27 <mohsin.h@turing.com>
2025-07-16 11:03:04 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Setup-Package",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Saad Habib",
+    description="Version management for Colab notebooks",
+    url="https://github.com/yourusername/Setup_Package",
+    python_requires='>=3.6',
+)
