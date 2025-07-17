58fe8b9f1e876ba3cbcaf1e8f2cee5bdcb5a5d37
Mohsin-h27 <mohsin.h@turing.com>
2025-07-17 10:23:23 -0700

    Update config.py

--- setup_package/config.py
+++ setup_package/config.py
@@ -4,15 +4,20 @@
 
 # --- Main Control Switch ---
 # If True: The script will IGNORE the Colab ID and use `GLOBAL_VERSION`.
-# If False: The script will read the Google Sheet using the ID below.
+# If False: The script will read the Google Sheet using the ID and Tab Name below.
 SWITCH = False # <-- Set to False to enable reading the Google Sheet
 
 # --- Global Version Setting ---
 # This is only used when SWITCH is True.
-GLOBAL_VERSION = "0.0.6"
+GLOBAL_VERSION = "0.0.8"
 
 # --- Google Sheet Configuration ---
 # This is only used when SWITCH is False.
+
 # Find this ID in the URL of your Google Sheet.
 # Example URL: https://docs.google.com/spreadsheets/d/THIS_IS_THE_ID/edit
-SHEET_ID = "1woSzszD4WoJPNQqfQ-o0Pg2GwTT1En-4z5e2T1R1Tlw" # <-- REPLACE WITH YOUR ACTUAL SHEET ID
+SHEET_ID = "14neNkLpLNhtYntcv8vipyreP7y1aq16M9" # <-- REPLACE WITH YOUR ACTUAL SHEET ID
+
+# The exact name of the tab (worksheet) inside your Google Sheet to read from.
+# For example: "Sheet1", "Latest Working Version", "Autorun_Results"
+SHEET_TAB_NAME = "Latest Working Version"
