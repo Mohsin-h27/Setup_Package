1280254230c5ccbc72bdb22362901c83b4b3ca56
Mohsin-h27 <mohsin.h@turing.com>
2025-07-17 04:48:20 -0700

    Update __init__.py

--- setup_package/__init__.py
+++ setup_package/__init__.py
@@ -1,4 +1,5 @@
-__version__ = "0.0.4"
-VERSION = __version__
+__version__ = "1.1.0"
 
-from .main import run_setup
+# This line makes the `main` function from main.py easy to import.
+# It's a shortcut so you can write `from setup_package import main` in your notebook.
+from .main import main
