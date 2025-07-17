ce3c0fb297e2c4240d79581a045cc67b140bcf04
Mohsin-h27 <mohsin.h@turing.com>
2025-07-17 04:47:26 -0700

    Create config.py

---
+++ setup_package/config.py
@@ -0,0 +1,22 @@
+# setup_package/config.py
+
+# This file holds all your settings. Edit these values to change the script's behavior.
+
+# --- Main Control Switch ---
+# If True: The script will automatically use the `GLOBAL_VERSION` below.
+# If False: The script will ask for a Colab ID and use the `VERSION_MAPPING`.
+SWITCH = True
+
+# --- Global Version Setting ---
+# This is the version of the API FILES that will be installed when SWITCH is True.
+GLOBAL_VERSION = "0.0.8"
+
+# --- Specific Version Mapping ---
+# This dictionary maps a Colab ID to a specific API version.
+# This is only used when SWITCH is False.
+VERSION_MAPPING = {
+    '1001': '0.0.4',
+    '1002': '0.0.8',
+    '1003': '0.0.1',
+    # Add more Colab IDs and their corresponding versions here
+}
